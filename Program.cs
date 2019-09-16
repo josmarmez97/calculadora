2415a8fdd4bb69600442a12aff902c9315a56819
josmarmez97 <43098299+josmarmez97@users.noreply.github.com>
2019-09-16 14:41:55 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            int x=2, y=5, resultado;
+            String sl=System.Environment.NewLine;
+
+            resultado=x+y;
+
+            Console.WriteLine(sl+"Presentamos la calculadora del Equipo 2"+sl);
+            Console.WriteLine("Integrantes: "+sl+"Fernandez Padierna Eduardo Martin"+sl+"Mezeta Yupit Josmar David"+sl+"Puc Oxte Rolando"+sl+"Chulin Eduardo");
+            
+            Console.WriteLine(sl+"Opreraciones: Suma, resta, multiplicacion y division:"+sl+"Variables:"+sl+" X ="+x+sl+"Y="+y);
+            Console.WriteLine("En una suma el resultado es: "+resultado);
+            
+        }
+    }
+}
