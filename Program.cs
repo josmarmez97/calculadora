f3093caa3fd8aac6fd3c1be98db15e07b981e7d1
rolando0803 <54915826+rolando0803@users.noreply.github.com>
2019-09-18 14:39:52 -0500

    Update Program.cs
    se agrega la resta

--- Program.cs
+++ Program.cs
@@ -7,17 +7,20 @@ namespace Calculadora
         static void Main(string[] args)
         {
             //Console.WriteLine("Hello World!");
+            
+            int  a=10, b=4,resta;
             int x=2, y=5, resultado;
-            String sl=System.Environment.NewLine;
 
             resultado=x+y;
 
+            resta=a-b
+                
             Console.WriteLine(sl+"Presentamos la calculadora del Equipo 2"+sl);
             Console.WriteLine("Integrantes: "+sl+"Fernandez Padierna Eduardo Martin"+sl+"Mezeta Yupit Josmar David"+sl+"Puc Oxte Rolando"+sl+"Chulin Eduardo");
             
             Console.WriteLine(sl+"Opreraciones: Suma, resta, multiplicacion y division:"+sl+"Variables:"+sl+" X ="+x+sl+"Y="+y);
             Console.WriteLine("En una suma el resultado es: "+resultado);
-            
+            consola.Writeline("en una resta el resultado es:"+resta);
         }
     }
 }
